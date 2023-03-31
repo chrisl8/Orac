014b21e94abf41de4ea1f1057719a0df13a93888
Christen Lofland <christen@lofland.net>
2023-03-30 21:11:04 -0500

    Change pm2 startup to use hand installed Node instead of nvm based.

--- startpm2.sh
+++ startpm2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export NVM_DIR="${HOME}/.nvm"
-[ -s "$NVM_DIR/nvm.sh"   ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-cd "${HOME}/Orac" || exit
-pm2 start "${HOME}/Orac/node/pm2Config.json"
+
+export PATH=/home/mycroft/bin:/home/mycroft/bin/node/bin:${PATH}
+cd "/home/mycroft/Orac" || exit
+pm2 start "/home/mycroft/Orac/node/pm2Config.json"
