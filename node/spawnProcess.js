4125193d1b5c13ce0523e401071a329d0b30e9fc
Christen Lofland <christen@lofland.net>
2023-05-18 10:24:15 -0500

    Add option to tell spawn process function not to print errors.

--- node/spawnProcess.js
+++ node/spawnProcess.js
@@ -4,9 +4,10 @@ import { spawn } from 'child_process';
  * An async Promise function
  * @param {String} path
  * @param args
+ * @param silent
  * @returns {Promise<unknown>}
  */
-function spawnProcess({ path, args = [] }) {
+function spawnProcess({ path, args = [], silent }) {
   // *** Return the promise
   return new Promise((resolve, reject) => {
     const process = spawn(path, args);
@@ -15,7 +16,9 @@ function spawnProcess({ path, args = [] }) {
       outputData += data;
     });
     process.stderr.on('data', (data) => {
-      console.error(String(data));
+      if (!silent) {
+        console.error(String(data));
+      }
     });
     process.on('close', (code) => {
       if (code > 0) {
