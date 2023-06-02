e5f66eb27120b81c2da3f95392bc7e638c2e0b84
Christen Lofland <christen@lofland.net>
2023-06-02 14:57:05 -0500

    Add CLI interface to pushMe.js

--- node/pushMe.js
+++ node/pushMe.js
@@ -1,6 +1,7 @@
 // Send message via Pushover API, which is free!
-import Push from "pushover-notifications";
-import getPushoverConfig from "./getPushoverConfig.js";
+import Push from 'pushover-notifications';
+import esMain from 'es-main';
+import getPushoverConfig from './getPushoverConfig.js';
 
 const configObject = await getPushoverConfig();
 
@@ -18,3 +19,12 @@ function pushMe(text) {
 }
 
 export default pushMe;
+
+if (esMain(import.meta)) {
+  if (process.argv.length < 3) {
+    console.log('You must provide text for the message, like this:');
+    console.log(`node pushMe.js "test message"`);
+    process.exit();
+  }
+  pushMe(process.argv[2]);
+}
