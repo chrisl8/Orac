ba84d9f6e72485a0b32b1c3e5e387ba0c26400b2
Christen Lofland <christen@lofland.net>
2023-08-23 08:24:11 -0500

    Website display WIP

--- website-source/src/routes/home.jsx
+++ website-source/src/routes/home.jsx
@@ -4,9 +4,21 @@ import perSiteSettings from '../utils/perSiteSettings';
 const Home = (props) => (
   <>
     <p>Welcome to {perSiteSettings.applicationName}.</p>
-    <p>Please choose an option from the menu on the left.</p>
-    <p>{props.siteDataModel.status}</p>
-    <p>Home Assistant </p>
+    <p>Orac Status: {props.siteDataModel.status}</p>
+    {props.siteDataModel.userLocation &&
+      props.siteDataModel.userLocation.isHome && (
+        <p>
+          Christen <strong> is</strong> home.
+        </p>
+      )}
+    {(!props.siteDataModel.userLocation ||
+      !props.siteDataModel.userLocation.isHome) && (
+      <p>
+        Christen is <strong> not</strong> home.
+      </p>
+    )}
+    {/*<p>Christen is home: {props.siteDataModel.userLocation.isHome}</p>*/}
+    <pre>{JSON.stringify(props.siteDataModel, null, 2)}</pre>
   </>
 );
 
