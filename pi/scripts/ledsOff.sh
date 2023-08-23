1addff3918393bd05e0edb7a5f83906b3cb67418
Christen Lofland <christen@lofland.net>
2023-08-23 08:25:34 -0500

    Add Mycroft hardware LED code.

--- pi/scripts/ledsOff.sh
+++ pi/scripts/ledsOff.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# https://github.com/MycroftAI/mark-ii-hardware-testing/blob/main/hardware_tests/ledtest_old.py
+# cmd =   "i2cset -y 1 %d %d %d %d %d i " % (
+#             self.device_addr,
+#             pixel,
+#             red_val,
+#             green_val,
+#             blue_val)
+
 /usr/sbin/i2cset -a -y 1 0x04 0 0 0 0 i
 /usr/sbin/i2cset -a -y 1 0x04 1 0 0 0 i
 /usr/sbin/i2cset -a -y 1 0x04 2 0 0 0 i
