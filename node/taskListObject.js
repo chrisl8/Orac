93bd8418dc694a73d3d7ecdb7a4551a9deab438b
Christen Lofland <christen@lofland.net>
2023-08-24 12:58:53 -0500

    Update personal tasks.

--- node/taskListObject.js
+++ node/taskListObject.js
@@ -1,15 +1,55 @@
 const taskList = {
+  meditate: {
+    interval: 'daily',
+    reminderAfterHour: 7,
+    reminderAfterMinute: 0,
+    repeatInterval: 60, // minutes
+    message: 'Meditation',
+    speakDo: 'Knock on the sky.',
+    speakDone: 'Zen plus plus!',
+    completedTexts: ['meditation', 'meditate'],
+    trellisButton: 30,
+    trellisButtonColor: [0, 0, 255],
+    pushoverExpirationTime: 60 * 10,
+  },
   pushUps: {
     interval: 'daily',
     reminderAfterHour: 7,
     reminderAfterMinute: 30,
     repeatInterval: 30, // minutes
-    message: 'Please do your push ups. -RED- 8',
+    message: 'Please do your push ups.',
     speakDo: "Don't forget to do your pushups today.",
     speakDone: 'Pushups done!',
-    completedTexts: ['push ups'],
+    completedTexts: ['push up', 'pushup', 'push-up'],
     trellisButton: 8,
     trellisButtonColor: [255, 0, 0],
+    pushoverExpirationTime: 60 * 10,
+  },
+  hollowHolds: {
+    interval: 'daily',
+    reminderAfterHour: 9,
+    reminderAfterMinute: 30,
+    repeatInterval: 30, // minutes
+    message: 'Hollow holds',
+    speakDo: 'Crunch time!',
+    speakDone: 'Yay!',
+    completedTexts: ['crunch', 'hollow hold', 'hollowhold'],
+    trellisButton: 9,
+    trellisButtonColor: [255, 255, 0],
+    pushoverExpirationTime: 60 * 10,
+  },
+  pullUps: {
+    interval: 'daily',
+    reminderAfterHour: 10,
+    reminderAfterMinute: 30,
+    repeatInterval: 30, // minutes
+    message: 'Pullups',
+    speakDo: 'Hang time!',
+    speakDone: 'Who raw!',
+    completedTexts: ['pullup', 'pull up'],
+    trellisButton: 10,
+    trellisButtonColor: [255, 0, 255],
+    pushoverExpirationTime: 60 * 10,
   },
   bike: {
     interval: 'daily',
@@ -20,20 +60,9 @@ const taskList = {
     speakDo: 'Time for some pedal pushing.',
     speakDone: 'Good job!',
     completedTexts: ['bike'],
-    trellisButton: 9,
+    trellisButton: 11,
     trellisButtonColor: [0, 255, 0],
-  },
-  meditate: {
-    interval: 'daily',
-    reminderAfterHour: 7,
-    reminderAfterMinute: 0,
-    repeatInterval: 60, // minutes
-    message: 'Meditation. -Blue- 30',
-    speakDo: 'Knock on the sky.',
-    speakDone: 'Zen plus plus!',
-    completedTexts: ['meditation'],
-    trellisButton: 30,
-    trellisButtonColor: [0, 0, 255],
+    pushoverExpirationTime: 60 * 10,
   },
 };
 
