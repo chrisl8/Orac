bd25e824c1750d38e74c133f15a8d68743f6c4c9
Christen Lofland <christen@lofland.net>
2023-08-28 17:06:24 -0500

    Add reminders to charge and wear watch.

--- node/taskListObject.js
+++ node/taskListObject.js
@@ -1,4 +1,45 @@
+/*
+Neopixel color helps
+https://learn.adafruit.com/adafruit-kb2040/neopixel-led
+
+red: (255, 0, 0)
+green: (0, 255, 0)
+blue: (0, 0, 255)
+cyan: (0, 255, 255)
+purple: (255, 0, 255)
+yellow: (255, 255, 0)
+white: (255, 255, 255)
+black (off): (0, 0, 0)
+ */
 const taskList = {
+  chargeWatch: {
+    interval: 'daily',
+    reminderAfterHour: 10,
+    reminderAfterMinute: 0,
+    repeatInterval: 60 * 4, // minutes
+    message: 'Charge Watch',
+    speakDo: 'Charge your watch if it needs it',
+    speakDone: 'Zap',
+    completedTexts: ['charg'],
+    trellisButton: 6,
+    trellisButtonColor: [255, 255, 125],
+    pushoverExpirationTime: 60 * 60 * 4, // seconds
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
+  putOnWatch: {
+    interval: 'daily',
+    reminderAfterHour: 11,
+    reminderAfterMinute: 0,
+    repeatInterval: 60, // minutes
+    message: 'Wear Watch',
+    speakDo: 'Is your watch done charging?',
+    speakDone: 'Thank you',
+    completedTexts: ['wear'],
+    trellisButton: 7,
+    trellisButtonColor: [0, 255, 255],
+    pushoverExpirationTime: 60 * 30, // seconds
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
   meditate: {
     interval: 'daily',
     reminderAfterHour: 7,
@@ -72,7 +113,7 @@ const taskList = {
     interval: 'daily',
     reminderAfterHour: 12,
     reminderAfterMinute: 0,
-    repeatInterval: 1, // minutes
+    repeatInterval: 15, // minutes
     message: 'Time to do 15 on the bike.',
     speakDo: 'Time for some pedal pushing.',
     speakDone: 'Good job!',
