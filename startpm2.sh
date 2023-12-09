47ed60d314e4a0e3d5bf2d3e8a26f78d7948cde0
Christen Lofland <christen@lofland.net>
2023-12-09 17:21:10 -0600

    Start pulseaudio before starting Orac so that sound works.

--- startpm2.sh
+++ startpm2.sh
@@ -3,6 +3,11 @@ export NVM_DIR="${HOME}/.nvm"
 # shellcheck source=/home/chrisl8/.nvm/nvm.sh
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
+# Pulseaudio is not starting automatically until a user logs in.
+# https://forums.raspberrypi.com/viewtopic.php?t=156120
+/usr/bin/pulseaudio --start --verbose
+/usr/bin/amixer -q -M sset Master 100%
+
 cd "${HOME}/Orac" || exit
 pm2 start "${HOME}/Orac/node/pm2Config.json"
 # If you want the terminal to stay open and show log, use this:
