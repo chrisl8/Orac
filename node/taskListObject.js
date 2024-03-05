4dca6585f9a7573862eee36309fa1c1ba49e42c7
Christen Lofland <christen@lofland.net>
2024-03-05 12:52:04 -0600

    Rearrange tasks.

--- node/taskListObject.js
+++ node/taskListObject.js
@@ -12,23 +12,72 @@ white: (255, 255, 255)
 black (off): (0, 0, 0)
  */
 const taskList = {
+  bike: {
+    interval: 'daily',
+    reminderAfterHour: 7,
+    reminderAfterMinute: 30,
+    repeatInterval: 90, // minutes
+    message: 'Time to do 15 on the bike.',
+    completedTexts: ['bike'],
+    trellisButton: 0,
+    trellisButtonColor: [0, 0, 255],
+    pushoverExpirationTime: 60 * 10,
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
+  hollowHolds: {
+    interval: 'daily',
+    reminderAfterHour: 7,
+    reminderAfterMinute: 30,
+    repeatInterval: 90, // minutes
+    message: 'Hollow holds',
+    completedTexts: ['crunch', 'hollow hold', 'hollowhold'],
+    trellisButton: 1,
+    trellisButtonColor: [255, 255, 0],
+    pushoverExpirationTime: 60 * 10,
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
+  pushUps: {
+    interval: 'daily',
+    reminderAfterHour: 7,
+    reminderAfterMinute: 30,
+    repeatInterval: 90, // minutes
+    message: 'Please do your push ups.',
+    completedTexts: ['push up', 'pushup', 'push-up'],
+    trellisButton: 2,
+    trellisButtonColor: [255, 0, 0],
+    pushoverExpirationTime: 60 * 10,
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
   meditate: {
     interval: 'daily',
-    reminderAfterHour: 8,
-    reminderAfterMinute: 0,
+    reminderAfterHour: 7,
+    reminderAfterMinute: 30,
     repeatInterval: 60, // minutes
     todoListEntryText: 'Meditate',
     message: 'Meditation',
     completedTexts: ['meditation', 'meditate'],
-    trellisButton: 30,
-    trellisButtonColor: [0, 0, 255],
+    trellisButton: 3,
+    trellisButtonColor: [255, 255, 255],
+    pushoverExpirationTime: 60 * 10,
+    daysOfTheWeek: [1, 2, 3, 4, 5],
+  },
+  duoLingo: {
+    interval: 'daily',
+    reminderAfterHour: 7,
+    reminderAfterMinute: 30,
+    repeatInterval: 60, // minutes
+    todoListEntryText: 'Duolingo',
+    message: 'Duolingo',
+    completedTexts: ['duoLingo'],
+    trellisButton: 4,
+    trellisButtonColor: [0, 255, 0],
     pushoverExpirationTime: 60 * 10,
     daysOfTheWeek: [1, 2, 3, 4, 5],
   },
   vitamin: {
     interval: 'daily',
-    reminderAfterHour: 8,
-    reminderAfterMinute: 30,
+    reminderAfterHour: 12,
+    reminderAfterMinute: 0,
     repeatInterval: 90, // minutes
     todoListEntryText: 'Take Daily Vitamin',
     message: "Don't forget to take your vitamin today!",
@@ -37,76 +86,28 @@ const taskList = {
     trellisButtonColor: [0, 255, 0],
     pushoverExpirationTime: 60 * 10,
   },
-  pushUps: {
-    interval: 'daily',
-    reminderAfterHour: 9,
-    reminderAfterMinute: 0,
-    repeatInterval: 90, // minutes
-    message: 'Please do your push ups.',
-    completedTexts: ['push up', 'pushup', 'push-up'],
-    trellisButton: 8,
-    trellisButtonColor: [255, 0, 0],
-    pushoverExpirationTime: 60 * 10,
-    daysOfTheWeek: [1, 2, 3, 4, 5],
-  },
-  hollowHolds: {
-    interval: 'daily',
-    reminderAfterHour: 9,
-    reminderAfterMinute: 30,
-    repeatInterval: 90, // minutes
-    message: 'Hollow holds',
-    completedTexts: ['crunch', 'hollow hold', 'hollowhold'],
-    trellisButton: 9,
-    trellisButtonColor: [255, 255, 0],
-    pushoverExpirationTime: 60 * 10,
-    daysOfTheWeek: [1, 2, 3, 4, 5],
-  },
   chargeWatch: {
     interval: 'daily',
-    reminderAfterHour: 10,
+    reminderAfterHour: 13,
     reminderAfterMinute: 0,
     repeatInterval: 60 * 4, // minutes
     message: 'Charge Watch',
     completedTexts: ['charg'],
-    trellisButton: 6,
+    trellisButton: 22,
     trellisButtonColor: [255, 255, 125],
     pushoverExpirationTime: 60 * 60 * 4, // seconds
     daysOfTheWeek: [1, 2, 3, 4, 5],
   },
-  pullUps: {
-    interval: 'daily',
-    reminderAfterHour: 10,
-    reminderAfterMinute: 30,
-    repeatInterval: 90, // minutes
-    message: 'Pullups',
-    completedTexts: ['pullup', 'pull up'],
-    trellisButton: 10,
-    trellisButtonColor: [255, 0, 255],
-    pushoverExpirationTime: 60 * 10,
-    daysOfTheWeek: [1, 2, 3, 4, 5],
-  },
   putOnWatch: {
     interval: 'daily',
-    reminderAfterHour: 11,
+    reminderAfterHour: 14,
     reminderAfterMinute: 0,
     repeatInterval: 60, // minutes
     message: 'Wear Watch',
     completedTexts: ['wear'],
-    trellisButton: 7,
+    trellisButton: 23,
     trellisButtonColor: [0, 255, 255],
     pushoverExpirationTime: 60 * 30, // seconds
-    daysOfTheWeek: [1, 2, 3, 4, 5],
-  },
-  bike: {
-    interval: 'daily',
-    reminderAfterHour: 12,
-    reminderAfterMinute: 0,
-    repeatInterval: 90, // minutes
-    message: 'Time to do 15 on the bike.',
-    completedTexts: ['bike'],
-    trellisButton: 11,
-    trellisButtonColor: [0, 255, 0],
-    pushoverExpirationTime: 60 * 10,
     daysOfTheWeek: [1, 2, 3, 4, 5],
   },
 };
